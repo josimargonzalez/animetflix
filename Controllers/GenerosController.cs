b37fa19a66bf2bd33d009709ac57e35d07e21bb2
Josimar Gonzalez <josimar.gonzalez@dafiti.cl>
2020-10-14 13:09:09 -0300

    crud for gender its finished

--- Controllers/GenerosController.cs
+++ Controllers/GenerosController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using animetflix.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,8 +18,66 @@ namespace animetflix.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var listaGeneros = await _context.Generos.ToListAsync();
+            var listaGeneros = await _context.Generos.OrderBy(a => a.Nombre).ToListAsync();
             return View(listaGeneros);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Nombre")] Genero genero )
+        {
+            //Valida si las reglas del modelo se cumplen o no
+            if (ModelState.IsValid)
+            {
+                _context.Add(genero);
+                await _context.SaveChangesAsync();
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(genero);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var genero = await _context.Generos.FindAsync(id);
+            
+            return View(genero);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre")] Genero genero )
+        {
+            //Valida si las reglas del modelo se cumplen o no
+            if (ModelState.IsValid)
+            {
+                _context.Update(genero);
+                await _context.SaveChangesAsync();
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(genero);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var genero = await _context.Generos.FindAsync(id);
+            
+            return View(genero);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, [Bind("Id,Nombre")] Genero genero )
+        {
+            _context.Remove(genero);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Models/Entities/Genero.cs
+++ Models/Entities/Genero.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace animetflix.Models.Entities
     public class Genero
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [MinLength(3, ErrorMessage = "El campo {0} debe tener al menos 3 caracteres")]
         public string Nombre { get; set; }
     }
 }
